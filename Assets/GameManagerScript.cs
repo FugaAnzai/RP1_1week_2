3f43dcfffa4f3e6a7d9eb2e3ad9e5c3b4d04469b
FugaAnzai <k022g0108@g.neec.ac.jp>
2023-05-24 17:19:59 +0900

    全消去

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -4,18 +4,11 @@ using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
-    public int hoge = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        hoge = 4;
-        int kyougou = 0;
-        kyougou = 2;
-        int unko = 0;
-        int ataoka = 0;
-        int ahomi = 4;
-        int sakiren = 5;
+
     }
 
     // Update is called once per frame
